83d853d92047823e485445ee461ca399ab367995
Ajit Das <ajitdas2900@gmail.com>
2017-02-09 20:43:23 +0600

    stripe integration added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Cart;
 use App\Product;
 use Illuminate\Http\Request;
 use Session;
+use Stripe\Charge;
+use Stripe\Stripe;
 
 class ProductController extends Controller
 {
@@ -32,5 +34,39 @@ class ProductController extends Controller
         $oldCart = Session::get('cart');
         $cart = new Cart($oldCart);
         return view('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+    }
+
+    public function getCheckout(){
+        if(!Session::has('cart')) {
+            return view('shop.shopping-cart', ['products' => null]);
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        $total = $cart->totalPrice;
+        return view('shop.checkout', ['total' => $total]);
+    }
+
+    public function postCheckout(Request $request){
+        if(!Session::has('cart')){
+            return redirect()->route('shop.shopping-cart');
+        }
+
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+
+        Stripe::setApiKey('sk_test_10wNdkClF4yGZ59EUBhPyhrY');
+        try {
+            Charge::create(array(
+                "amount" => $cart->totalPrice * 100,
+                "currency" => "usd",
+                "source" => $request->input('stripeToken'), // obtained with Stripe.js
+                "description" => "Test Charge"
+            ));
+        } catch (\Exception $e) {
+            return redirect()->route('checkout')->with('error', $e->getMessage());
+
+        }
+        Session::forget('cart');
+        return redirect()->route('product.index')->with('success', 'Successfully purchased products');
     }
 }
--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2/8/2017
- * Time: 9:57 PM
- */
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping Cart
+@endsection
+@section('content')
+    @if(Session::has('cart'))
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <ul class="list-group">
+                    @foreach($products as $product)
+                        <li class="list-group-item">
+                            <span class="badge">{{ $product['qty'] }}</span>
+                            <strong>{{ $product['item']['title'] }}</strong>
+                            <span class="label label-success"> {{ $product['price'] }}</span>
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primay btn-xs dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span></button>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Reduce by 1</a></li>
+                                    <li><a href="#">Reduce all</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <strong>Total: {{ $totalPrice }}</strong>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <a href="{{ route('checkout') }}" type="button" class="btn btn-success">Checkout</a>
+            </div>
+        </div>
+    @else
+
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <h2>No Items in Cart !</h2>
+            </div>
+        </div>
+    @endif
+
+@endsection
